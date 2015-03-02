7c47f806867dc50960d9a6621bedc4e5b42d9bcd
MrJmolly <kaschei612@gmail.com>
2015-03-02 18:50:38 +0300

    update3
    добавил сплит и класс

--- src/myWork.java
+++ src/myWork.java
@@ -21,6 +21,7 @@ public class myWork {
 				if (k>=n && k<n+i){
 					bw.write(line);
 					bw.newLine();
+					strDiv(line);
 				}
 				else if (k>n+i) break;
 			}
@@ -36,5 +37,34 @@ public class myWork {
 		catch (FileNotFoundException e){
 			System.out.print("File not found!!!");
 		}
+	}
+	public static void strDiv(String str){
+
+		strDivC s = new strDivC();
+		String[] parts = new String[4];
+		parts = str.split(" ");
+		s.host = parts[0];
+		s.time = parts[3]+" "+parts[4];
+		s.req = parts[5]+" "+parts[6]+" "+parts[7];
+		s.http = Integer.parseInt(parts[8]);
+		s.reply = Integer.parseInt(parts[9]);
+		strDivO(s);
+	}
+	public static void strDivO(strDivC s){
+		System.out.println("----------------------------");
+		System.out.println("Host: " + s.host);
+		System.out.println("Time: " + s.time);
+		System.out.println("Request: " + s.req);
+		System.out.println("Http reply code: " + s.http);
+		System.out.println("Bytes in reply: " + s.reply);
+	
+	}
+	private static class strDivC{
+		String host;
+		String time;
+		String req;
+		int http;
+		int reply;
+		
 	}
 }
